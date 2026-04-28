f8698198003f34c389607757a3a0dbf77676c1ed
100G PC <jaylen@sulfurdesign.com>
2026-04-28 16:38:37 +1000

    Working PWM Fan Control with Vivado
    we fucken did the fan control shit my guy, only other step for this is to build a way to control it.

--- VivadoFanTest.srcs/constrs_1/new/FanConstraints.xdc
+++ VivadoFanTest.srcs/constrs_1/new/FanConstraints.xdc
@@ -10,9 +10,5 @@ create_clock -period 1.600 [get_ports mgtrefclk0n]
 create_clock -period 6.400 [get_ports mgtrefclk1p]
 create_clock -period 6.400 [get_ports mgtrefclk1n]
 
-create_clock -period 6.000 -name eth_gt0_txoutclk [get_pins -hier -filter name=~*gt0_vx690tffg1157_i*gthe2_i*TXOUTCLK]
-create_clock -period 3.103 -name eth_gt0_rxoutclk [get_pins -hier -filter name=~*gt0_vx690tffg1157_i*gthe2_i*RXOUTCLK]
-create_clock -period 3.103 -name eth_gt1_rxoutclk [get_pins -hier -filter name=~*gt1_vx690tffg1157_i*gthe2_i*RXOUTCLK]
-create_clock -period 3.103 -name eth_gt2_rxoutclk [get_pins -hier -filter name=~*gt2_vx690tffg1157_i*gthe2_i*RXOUTCLK]
-create_clock -period 3.103 -name eth_gt3_rxoutclk [get_pins -hier -filter name=~*gt3_vx690tffg1157_i*gthe2_i*RXOUTCLK]
+
 
--- VivadoFanTest.srcs/sources_1/new/topLevel.vhd
+++ VivadoFanTest.srcs/sources_1/new/topLevel.vhd
@@ -18,24 +18,26 @@
 -- 
 ----------------------------------------------------------------------------------
 
+-- PWM code of sequencial logic for fpga.
+
+-- This code applies for chips working with 50MHz; 
+-- If you work with another frequency, change the constante prescaler according the equation above
+--	clk/(255*frequency)
+
+-- Autor: Rodolfo Cavour Moretti Schiavi
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_unsigned.all;
 
 entity top_level is
-    port (
-        -- 25 MHz input clock
+	port(
+		-- 25 MHz input clock
         clk25in       : in  std_logic;
 
+        -- 8 bits for 0 to 100% of duty cycle
+		-- duty_cycle: in std_logic_vector(7 downto 0) := "00100000"; 
+		
         -- fan control output
         fan           : out std_logic;
 
@@ -44,25 +46,42 @@ entity top_level is
         mgtrefclk0n   : in  std_logic;
         mgtrefclk1p   : in  std_logic;
         mgtrefclk1n   : in  std_logic
-    );
-end entity top_level;
-architecture rtl of top_level is
+		);
+end top_level;
 
-    -- Internal signals for future logic
-    signal fan_reg : std_logic := '0';
-
-begin
-
-    --------------------------------------------------------------------
-    -- Simple placeholder logic (replace with real control logic)
-    --------------------------------------------------------------------
-    process(clk25in)
-    begin
-        if rising_edge(clk25in) then
-            fan_reg <= not fan_reg;
-        end if;
-    end process;
-
-    fan <= fan_reg;
-
-end architecture rtl;
+architecture modulation of top_level is
+	signal signal_pwm: std_logic := '0';
+	
+BEGIN
+		PROCESS (clk25in) 
+		
+		constant prescaler: integer range 0 to 255 := 196; -- Clock divider for reach  1kHz, change this value 
+												           -- with the following equation: clk/(255*frequency)
+												           
+		constant duty_cycle: std_logic_vector(7 downto 0) := "10000000";
+		
+		variable t: integer range 0 to 32768 := 0;	       -- Time counter
+		variable t_mod: integer range 0 to 255;	           -- Time counter for the duty cycle
+		constant end_range: integer range 0 to 255 := 255;   -- Constante to produce the low signal
+		
+		BEGIN
+			if(rising_edge(clk25in)) then
+				t := t + 1;
+				if(prescaler = t) then 
+					t := 0;
+					t_mod:= t_mod + 1;
+					if(t_mod = 255) then
+						t_mod := 0;
+					end if;
+					if(t_mod <= CONV_INTEGER(duty_cycle)) then
+						signal_pwm <= '0';
+					end if;
+					if(t_mod >= end_range - CONV_INTEGER(duty_cycle)) then
+						signal_pwm <= '1';
+					end if;
+				end if;
+			end if;
+		end process;
+		fan <= signal_pwm;
+												
+end modulation;
